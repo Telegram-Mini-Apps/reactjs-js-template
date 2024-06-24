6513592a662c129358b96f4421d3c110c7fac157
Vladislav Kibenko <wolfram.deus@gmail.com>
2024-06-25 00:37:55 +0300

    Refactor vite config

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,30 @@
-// import { readFileSync } from 'node:fs';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-js-template/',
-  plugins: [react()],
-  // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
-  // you have key and certificate. You retrieve your certificate and key using mkcert.
-  // Learn more:
-  // https://docs.telegram-mini-apps.com/platform/getting-app-link#mkcert
-  //
-  // server: {
-  //   port: 443,
-  //   https: {
-  //     cert: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal.pem')),
-  //     key: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal-key.pem')),
-  //   },
-  //   host: 'tma.internal',
-  // },
+  base: '/reactjs-js-template',
+  plugins: [
+    // Allows using React dev server along with building a React application with Vite.
+    // https://npmjs.com/package/@vitejs/plugin-react-swc
+    react(),
+    // Allows using self-signed certificates to run the dev server using HTTPS.
+    // https://www.npmjs.com/package/@vitejs/plugin-basic-ssl
+    // basicSsl(),
+  ],
+  publicDir: './public',
+  server: {
+    // Uncomment this line if you want to expose your dev server and access it from the devices
+    // in the same network.
+    // host: true,
+  },
   resolve: {
     alias: {
       '@': resolve(dirname(fileURLToPath(import.meta.url)), './src'),
     }
   },
-  publicDir: './public',
 });
 
